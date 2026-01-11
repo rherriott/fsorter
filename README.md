8faef0633e95773028c2d2f07afd175e47d0ef2e
rherriott <fdsin0wrench@gmail.com>
2026-01-11 11:06:30 -0800

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+##### FSorter
+
+The purpose of the program that follows is to easily parse through files (for example, images), and sort them into sub-folders.
---
+++ fsorter.py
@@ -0,0 +1,17 @@
+
+# FSorter
+
+import asyncio
+#from typing import TypedDict
+
+#class Arguments(TypedDict):
+#  pass
+
+def parse() -> dict:
+  return {}
+
+async def act(args: dict) -> None:
+  return
+
+if __name__ == "__main__":
+  asyncio.run(act(parse()))
